594330277bf08ec3967fdc27db77cc40c77e1571
fredvunkiktkhk <fred.vunk@khk.ee>
2022-06-12 22:54:40 +0300

    Final solution

--- readMe.md
+++ readMe.md
@@ -1,5 +1,8 @@
-Might need for db querys with user
-###docker exec -it db bash
-###mysql -h localhost -P 3306 --protocol=tcp -uroot -pMYSQL_ROOT_PASSWORD
-###grant ALL PRIVILEGES ON *.* TO 'MYSQL_USER';
-###flush privileges
+To start project use command:
+###docker-compose up
+
+If needed to give user privileges for db querys as user
+### docker exec -it db bash
+### mysql -h localhost -P 3306 --protocol=tcp -uroot -pMYSQL_ROOT_PASSWORD
+### grant ALL PRIVILEGES ON *.* TO 'MYSQL_USER';
+### flush privileges
--- src/register.php
+++ src/register.php
@@ -4,41 +4,43 @@
 <head>
     <title>Show databases in MySQL server</title>
     <meta http-equiv="content-type" content="text/html; charset=iso-8859-1"/>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
 </head>
 <body>
 <div class="container">
     <h3>Please create a username to save your data.</h3>
 
-  <?php
-  $nameError = "";
+    <?php
+    $nameError = "";
+    $userNameExistsError = "";
+    $err = "<p class='text-warning'></p>";
 
-  if (isset($_POST['register'])) {
-    if (empty($_POST['username'])) {
-      $nameError = "Username is required";
-      echo $nameError;
-    } else {
+    if (isset($_POST['register'])) {
+        if (empty($_POST['username'])) {
+            $nameError = "Username is required";
+            echo "<p class='text-danger'>$nameError</p>";
+        } else {
 
-      $conn = require "conn.php";
-      $username = $_POST['username'];
-      $usernameError = "";
-      $errors = array();
+            $conn = require "conn.php";
+            $username = $_POST['username'];
+            $checkIfUsernameExists = $conn->prepare("SELECT username FROM username WHERE username = :username");
+            $checkIfUsernameExists->execute(['username' => $username]);
+            $user = $checkIfUsernameExists->fetch();
 
-      $checkIfUsernameExists = $conn->prepare("SELECT username FROM username WHERE username = :username");
-      $checkIfUsernameExists->execute(['username' => $username]);
-      $user = $checkIfUsernameExists->fetch();
+            if ($user['username'] === $username) {
+                $userNameExistsError = "Username already exists";
+                echo "<p class='text-danger'>$userNameExistsError</p>";;
+            } else {
+                $query = $conn->prepare("INSERT INTO username (username) VALUES (:username)");
+                $query->execute(['username' => $username]);
+                session_start();
+                $_SESSION['active_user'] = $username;
 
-      if ($user['username'] === $username) {
-        array_push($errors, "Username already exists");
-        echo "Username already exists";
-      } else {
-        $query = $conn->prepare("INSERT INTO username (username) VALUES (:username)");
-        $query->execute(['username' => $username]);
-        header("Location: user-data.php");
-      }
+                header("Location: sectors.php");
+            }
+        }
     }
-  }
-  ?>
+    ?>
     <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>">
         <label>Username</label>
         <input type="text" name="username"/>
